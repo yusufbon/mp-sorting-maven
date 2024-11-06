711466aac7b8c1118abf7945e51ab1a42e4fa5b8
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-11-06 15:37:47 -0600

    Add InsertionSorter.

---
+++ src/main/java/edu/grinnell/csc207/sorting/InsertionSorter.java
@@ -0,0 +1,68 @@
+package edu.grinnell.csc207.sorting;
+
+import java.util.Comparator;
+
+/**
+ * Something that sorts using insertion sort.
+ * tests fail.
+ *
+ * @param <T>
+ *   The types of values that are sorted.
+ *
+ * @author Samuel A. Rebelsky
+ */
+
+public class InsertionSorter<T> implements Sorter<T> {
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The way in which elements are ordered.
+   */
+  Comparator<? super T> order;
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Create a sorter.
+   */
+  public InsertionSorter() {
+  } // InsertionSorter()
+
+  /**
+   * Create a sorter using a particular comparator (included for
+   * consistency with other sorters).
+   *
+   * @param comparator
+   *   The order in which elements in the array should be ordered
+   *   after sorting.
+   */
+  public InsertionSorter(Comparator<? super T> comparator) {
+    this.order = comparator;
+  } // InsertionSorter(Comparator)
+
+  // +---------+-----------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  /**
+   * Sort an array in place using insertion sort.
+   *
+   * @param values
+   *   an array to sort.
+   *
+   * @post
+   *   The array has been sorted according to some order (often
+   *   one given to the constructor).
+   * @post
+   *   For all i, 0 &lt; i &lt; vals.length,
+   *     order.compare(vals[i-1], vals[i]) &lt;= 0
+   */
+  @Override
+  public void sort(T[] values) {
+    // STUB
+  } // sort(T[])
+} // class InsertionSorter
