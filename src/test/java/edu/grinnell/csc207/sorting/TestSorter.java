f163beacd14728ca8149d2c1839cd420746d2258
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-11-06 18:42:47 -0600

    Update TestSorter: Better error messages, new test

--- src/test/java/edu/grinnell/csc207/sorting/TestSorter.java
+++ src/test/java/edu/grinnell/csc207/sorting/TestSorter.java
@@ -1,12 +1,15 @@
 package edu.grinnell.csc207.sorting;
 
+import edu.grinnell.csc207.util.ArrayUtils;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
-import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -33,6 +36,33 @@ public class TestSorter {
    */
   static Sorter<Integer> intSorter = null;
 
+  // +-----------+---------------------------------------------------
+  // | Utilities |
+  // +-----------+
+
+  /**
+   * Given a sorted array and a permutation of the array, sort the
+   * permutation and assert that it equals the original.
+   *
+   * @param <T>
+   *   The type of values in the array.
+   * @param sorted
+   *   The sorted array.
+   * @param perm
+   *   The permuted sorted array.
+   * @param sorter
+   *   The thing to use to sort.
+   */
+  public <T> void assertSorts(T[] sorted, T[] perm, Sorter<? super T> sorter) {
+    T[] tmp = perm.clone();
+    sorter.sort(perm);
+    assertArrayEquals(sorted, perm,
+      () -> String.format("sort(%s) yields %s rather than %s",
+          Arrays.toString(tmp), 
+          Arrays.toString(perm), 
+          Arrays.toString(sorted)));
+  } // assertSorts
+
   // +-------+-------------------------------------------------------
   // | Tests |
   // +-------+
@@ -56,8 +86,7 @@ public class TestSorter {
     } // if
     String[] original = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
     String[] expected = original.clone();
-    stringSorter.sort(original);
-    assertArrayEquals(original, expected);
+    assertSorts(expected, original, stringSorter);
   } // orderedStringTest
 
   /**
@@ -70,8 +99,25 @@ public class TestSorter {
     } // if
     String[] original = { "foxtrot", "delta", "charlie", "bravo", "alpha" };
     String[] expected = { "alpha", "bravo", "charlie", "delta", "foxtrot" };
-    stringSorter.sort(original);
-    assertArrayEquals(original, expected);
+    assertSorts(expected, original, stringSorter);
   } // orderedStringTest
 
+  /**
+   * Ensure that a randomly permuted version of a moderate-sized
+   * array sorts correctly.
+   */
+  @Test 
+  public void permutedIntegersTest() { 
+    int SIZE = 100; 
+    if (null == intSorter) { 
+      return; 
+    } // if
+    Integer[] original = new Integer[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+      original[i] = i;
+    } // for
+    Integer[] expected = original.clone();
+    ArrayUtils.permute(original);
+    assertSorts(expected, original, intSorter);
+  } // permutedIntegers
 } // class TestSorter
