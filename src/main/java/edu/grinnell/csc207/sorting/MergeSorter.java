477adbc45590b285211d11ee0baa70e0df87382d
yusufbon <yusufbonsen@gmail.com>
2024-12-16 21:19:18 -0600

    Update MergeSorter.java

--- src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
@@ -1,5 +1,6 @@
 package edu.grinnell.csc207.sorting;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
@@ -41,6 +42,33 @@ public class MergeSorter<T> implements Sorter<T> {
   // +---------+
 
   /**
+   * Merges two sorted arrays into a single sorted array.
+   *
+   * @param result the array to store the merged result.
+   * @param left   the left sorted subarray.
+   * @param right  the right sorted subarray.
+   */
+  private void merge(T[] result, T[] left, T[] right) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    while (i < left.length && j < right.length) {
+      if (order.compare(left[i], right[j]) <= 0) {
+        result[k++] = left[i++];
+      } else {
+        result[k++] = right[j++];
+      } //else
+    } //while
+    while (i < left.length) {
+      result[k++] = left[i++];
+    } //while
+    while (j < right.length) {
+      result[k++] = right[j++];
+    } //while
+  } //merge(T[] result, T[] left, T[] right)
+
+
+  /**
    * Sort an array in place using merge sort.
    *
    * @param values
@@ -55,6 +83,11 @@ public class MergeSorter<T> implements Sorter<T> {
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    if (values.length <= 1) return;
+    T[] left = Arrays.copyOfRange(values, 0, values.length / 2);
+    T[] right = Arrays.copyOfRange(values, values.length / 2, values.length);
+    sort(left);
+    sort(right);
+    merge(values, left, right);
   } // sort(T[])
 } // class MergeSorter
